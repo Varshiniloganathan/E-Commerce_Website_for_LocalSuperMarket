b89b4916015c44df34f3dd94bc6bd2bfb8504150
Varshiniloganathan <varshulogu04@gmail.com>
2025-02-09 21:44:30 +0530

    Homepage is updated with product details

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,23 @@
-const HomePage=()=>{
+import './HomePage.css';
+import React,{useEffect,useState} from "react";
+const HomePage=({addToCart})=>{
+    const [products, setProducts] = useState([]);
+    useEffect(()=>{
+        fetch("/data/products.json").then((response)=>response.json()).then((data)=>setProducts(data)).catch((error)=>console.error("Error loading products:",error));
+    },[]);
     return(
         <>
-        <h1>H1</h1>
+        <div className="product-container">
+            {products.map((product)=>(
+                <div key={product.id} className="product-card">
+                    <img src={product.image} alt={product.name} className="product-image"/>
+                    <h3>{product.name}</h3>
+                    <p>{product.description}</p>
+                    <p>₹{product.price}</p>
+                    <button onClick={()=>addToCart(product)}>Add to Cart</button>
+                </div>
+            ))}
+        </div>
         </>
     );
 };
