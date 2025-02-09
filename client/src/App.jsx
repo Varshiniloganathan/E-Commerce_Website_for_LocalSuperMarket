c6b51b683ffa77303465dcbbe7a7c93b7b2e6d7d
Varshiniloganathan <varshulogu04@gmail.com>
2025-02-09 23:29:22 +0530

    Cart page is updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Routes,Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
@@ -7,20 +7,27 @@ import Signin from './pages/Signin';
 import './App.css';
 //import Navbar from './components/Navbar';
 function App() {
+  const [cartItems,setCartItems] = useState([]);
+  const addToCart = (product) => {
+    setCartItems((prevItems)=>[...prevItems,product]);
+  };
+  const removeFromCart=(id)=>{
+    setCartItems(cartItems.filter(item=>item.id!==id));
+  }
 
   return (
     <>
     <div className='container'>
       <Navbar/>
       <Routes>
-      <Route path="/" element={<HomePage/>} />
-      <Route path="/CartPage" element={<CartPage/>}/>
+      <Route path="/" element={<HomePage addToCart={addToCart}/>} />
+      <Route path="/CartPage" element={<CartPage cartItems={cartItems} removeFromCart={removeFromCart}/>}/>
       <Route path="/Signin" element={<Signin/>}/>
     </Routes>
     </div>
     
     </>
-  )
-}
+  );
+};
 
 export default App;
